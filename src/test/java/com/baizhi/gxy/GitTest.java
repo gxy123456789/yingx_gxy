6b313231007121362bf2bd1b8cba369193609274
Guxy <630213168@qq.com>
2020-04-14 00:49:47 +0800

    添加

--- src/test/java/com/baizhi/gxy/GitTest.java
+++ src/test/java/com/baizhi/gxy/GitTest.java
@@ -2,8 +2,6 @@ package com.baizhi.gxy;
 
 import com.baizhi.gxy.dao.AdminDao;
 import com.baizhi.gxy.dao.UserMapper;
-import com.baizhi.gxy.entity.User;
-import com.baizhi.gxy.entity.UserExample;
 import com.baizhi.gxy.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,6 +28,7 @@ public class GitTest {
     public void contextLossads() {
         int a=10;
 
+        System.out.println("从IDEA写到Git上");
     }
 
 
